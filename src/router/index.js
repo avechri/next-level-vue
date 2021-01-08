17474d305682277be03291dfff0bd6b429e58abf
Kristina <christychri0@gmail.com>
2021-01-08 21:34:26 +0700

    Удалила старые элементы, заменила на новые. Настроила роутинг из меню

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,33 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+// import EventCreate from '../views/EventCreate.vue';
+import EventList from '../views/EventList.vue';
+// import EventShow from '../views/EventShow.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'event-list',
+    component: EventList,
   },
   {
-    path: '/about-us',
-    name: 'about',
+    path: '/event/:id',
+    name: 'event-show',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "event-show" */ '../views/EventShow.vue'),
+    props: true,
   },
   {
-    path: '/about',
-    redirect: { name: 'about' },
+    path: '/event/create',
+    name: 'event-create',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "event-create" */ '../views/EventCreate.vue'),
   },
 ];
 
