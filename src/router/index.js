d71f6cbe17c141455388fd44468eecdcfa4c1751
kristina <christychri0@gmail.com>
2021-03-17 13:06:01 +0700

    подключила NetworkIssue компонент в роутер

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import EventShow from '@/views/EventShow.vue';
 import NProgress from 'nprogress';
 import store from '@/store/index';
 import NotFound from '@/views/NotFound.vue';
+import NetworkIssue from '@/views/NetworkIssue.vue';
 import EventCreate from '../views/EventCreate.vue';
 import EventList from '../views/EventList.vue';
 
@@ -50,6 +51,12 @@ const router = new VueRouter({
       path: '/404',
       name: '404',
       component: NotFound,
+      props: true,
+    },
+    {
+      path: '/network-issue',
+      name: 'network-issue',
+      component: NetworkIssue,
       props: true,
     },
     {
