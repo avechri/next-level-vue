15b019fcccbb9057cfc46b58a94992a4b9723272
kristina <christychri0@gmail.com>
2021-03-18 21:48:38 +0700

    создала реюзабл компонент ввода, добавила базовые элементы

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <label v-if="label">{{ label }}}</label>
+  <input :value="value" @input="updateValue">
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    value: [String, Number],
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
