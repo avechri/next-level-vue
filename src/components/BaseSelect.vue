7322761e36f277489f8736d5c9f7cf66c0412b95
kristina <christychri0@gmail.com>
2021-03-23 19:51:46 +0700

    Создала BaseSelect.vue с минимальным функционалом (для отображения)

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,43 @@
+//BaseSelect comp needs:
+//label, select & option elements
+//props: label, value, options
+//ability to send up selected data
+// info is similar to BaseInput comp sp copy paste this code first & change what i need to change
+
+<template>
+  <div>
+    <label v-if="label">{{ label }}}</label>
+    <select :value="value" @input="updateValue" v-bind="$attrs">
+      <option
+        v-for="oprion in options"
+        :key="oprion"
+      >{{ option }}</option>>
+    </select>>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    value: [String, Number],
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
